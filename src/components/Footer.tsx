d6e84e12ff80db8078b638ef90ef8e8b39b85137
Anurodh <anurodhp7@gmail.com>
2023-07-03 09:39:50 +1000

    added footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <footer className="">
+      <small>
+        {/* Coded with ❤️ by{" "} */}
+        <a
+          className="footer--link"
+          href="https://anurodhpandey.com.np/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Anurodh Pandey
+        </a>
+        .
+      </small>
+    </footer>
+  );
+};
+
+export default Footer;
