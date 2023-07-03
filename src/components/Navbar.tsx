f1aaded9843a611d569131e4d64c7e0b474ab50d
Anurodh <anurodhp7@gmail.com>
2023-07-03 12:50:27 +1000

    changed the Title to myTravels

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,9 +5,7 @@ const Navbar = () => {
   return (
     <nav className=" bg-red-600 shadow-md py-3 px-0 flex justify-center gap-2 text-white">
       <FontAwesomeIcon icon={faEarthEurope} className="h-8" />
-      <h1 className="pt-1  font-semibold tracking-tighter">
-        my Travel Journal.
-      </h1>
+      <h1 className="pt-1  font-semibold tracking-tighter">my Travels.</h1>
     </nav>
   );
 };
