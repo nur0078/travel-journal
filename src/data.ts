96cb3e9205c9dd4aacea4b3a04ee6392e125b088
Anurodh <anurodhp7@gmail.com>
2023-07-03 12:48:11 +1000

    changes in data file

--- src/data.ts
+++ src/data.ts
@@ -1,36 +1,63 @@
 export const Data = () => [
+
   {
     key: 1,
-    title: "Mount Fuji",
-    location: "Japan",
-    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-    startDate: "12 Jan, 2021",
-    endDate: "24 Jan, 2021",
-    description:
-      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-    imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
-  },
-  {
-    key: 2,
     title: "Sydney Opera House",
     location: "Australia",
     googleMapsUrl: "https://goo.gl/maps/681n4jdijgdt3uGS6",
-    startDate: "27 May, 2021",
-    endDate: "8 Jun, 2021",
+    startDate: "01 Feb, 2020",
+    endDate: "15 March, 2027",
     description:
       "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
     imageUrl: "https://source.unsplash.com/JmuyB_LibRo",
   },
   {
-    key: 3,
-    title: "Geirangerfjord",
-    location: "Norway",
-    googleMapsUrl: "https://goo.gl/maps/5J6KAPeSgM8CHQwB7",
-    startDate: "01 Oct, 2021",
-    endDate: "18 Nov, 2021",
+    key: 2,
+    title: "Phewa Tal (Fewa Lake)",
+    location: "Nepal",
+    googleMapsUrl: "https://goo.gl/maps/oHHCu1gxx1uiCE7m6",
+    startDate: "27 May, 2021",
+    endDate: "8 Jun, 2021",
     description:
-      "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
-    imageUrl: "https://source.unsplash.com/3PeSjpLVtLg",
+      "Phewa Tal, also known as Fewa Lake, is a gorgeous freshwater lake located in Pokhara, the second-largest city of Nepal. The lake is famous for its stunning reflections of the nearby Annapurna Range, and on its eastern shore, visitors can explore the sacred Tal Barahi Temple, situated on an island in the lake.",
+    imageUrl: "https://source.unsplash.com/j5V55liwchk",
+
+  },
+
+  {
+    key: 3,
+    title: "Bali, Indonesia",
+    location: "Indonesia",
+    googleMapsUrl: "https://goo.gl/maps/G8NzTG2scARveQB89",
+    startDate: "12 Feb, 2021",
+    endDate: "24 Feb, 2021",
+    description:
+      "Bali, Indonesia is a world-renowned tropical paradise known for its lush landscapes, picturesque rice terraces, and tranquil beaches. The island offers a rich cultural experience with its unique arts scene, ancient temples, traditional ceremonies, and warm, welcoming locals.",
+    imageUrl: "https://source.unsplash.com/1kdIG_258bU",
+  },
+
+  {
+    key: 4,
+    title: "Mount Fuji",
+    location: "Japan",
+    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    startDate: "12 Mar, 2022",
+    endDate: "24 Mar, 2022",
+    description:
+      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+    imageUrl: "https://source.unsplash.com/WLxQvbMyfas",
+  },
+
+  {
+    key: 5,
+    title: "Mustang",
+    location: "Nepal",
+    googleMapsUrl: "https://goo.gl/maps/mbzXfegDxY2TjQ2F7",
+    startDate: "01 Nov, 2022",
+    endDate: "18 Nov, 2022",
+    description:
+      "Mustang, Nepal is a hidden treasure in the Himalayas, boasting dramatic landscapes with rugged mountain terrain, deep canyons, and a culturally rich Tibetan heritage. Known for its unique medieval wall city of Lo Manthang, centuries-old monasteries, and ancient Tibetan Buddhist shrines, Mustang offers a truly off-the-beaten-path experience.",
+    imageUrl: "https://source.unsplash.com/KM1QLHnxA4c",
   },
 ];
 
