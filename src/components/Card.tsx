85e042257194159943766f544a94cfbc42875889
Anurodh <anurodhp7@gmail.com>
2023-07-03 12:48:31 +1000

    syled card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -17,22 +17,34 @@ interface ICardProps {
 
 const Card = ({ data }: ICardProps) => {
   return (
-    <div className="'card">
-      <div className="card--img-box">
-        <img src={data.imageUrl} alt={data.title} />
+    <div className=" sm:flex-col md:flex-row py-8 px-0 gap-10 text-justify md:flex">
+      <div>
+        <img
+          className="h-full w-full aspect-video object-cover rounded-lg shadow-md transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-500 hover:shadow-xl md:w-80 "
+          src={data.imageUrl}
+          alt={data.title}
+        />
       </div>
-      <div className="card--infos">
-        <FontAwesomeIcon icon={faLocationDot} className="card--location-icon" />
-        <h2 className="card--location"> {data.location} </h2>
-        <a className="card--location-url" href={data.googleMapsUrl}>
+      <div className="p-0 mt-4 mx-0 mb-0">
+        <FontAwesomeIcon icon={faLocationDot} className=" text-red-500" />
+        <h2 className=" inline-block text-sm tracking-widest uppercase mt-0 mx-4">
+          {data.location}
+        </h2>
+        <a
+          className=" text-sm underline"
+          href={data.googleMapsUrl}
+          target="_blank"
+        >
           View on Google Maps
         </a>
 
-        <h3 className="card--title">{data.title}</h3>
-        <p className="card--date">
+        <h3 className="text-2xl pt-3 mx-0 font-bold">{data.title}</h3>
+        <p className="pt-2 font-semibold">
           {data.startDate} - {data.endDate}
         </p>
-        <p className="card--description">{data.description}</p>
+        <p className="max-w-3xl pt-3 leading-normal text-base font-normal ">
+          {data.description}
+        </p>
       </div>
     </div>
   );
